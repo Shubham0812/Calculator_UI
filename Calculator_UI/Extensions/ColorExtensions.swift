640cc8bf0a6f5fb981ea328e95811ee5566c8132
Shubham0812 <singh.shubham0812@gmail.com>
2021-07-13 18:34:57 +0530

    Extensions and Resources added

--- Calculator_UI/Extensions/ColorExtensions.swift
+++ Calculator_UI/Extensions/ColorExtensions.swift
@@ -2,7 +2,82 @@
 //  ColorExtensions.swift
 //  Calculator_UI
 //
-//  Created by Shubham on 11/07/21.
+//  Created by Shubham on 12/07/21.
 //
 
-import Foundation
+import SwiftUI
+
+extension Color {
+    
+    init(hex: String) {
+        self.init(UIColor(hex: hex))
+    }
+    
+    static func randomColor() -> Color {
+        return Color(UIColor.random())
+    }
+}
+
+extension UIColor {
+    /// For converting Hex-based colors
+    convenience init(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+        var rgb: UInt64 = 0
+        
+        var r: CGFloat = 0.0
+        var g: CGFloat = 0.0
+        var b: CGFloat = 0.0
+        var a: CGFloat = 1.0
+        
+        let length = hexSanitized.count
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+        
+        if length == 6 {
+            r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+            g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+            b = CGFloat(rgb & 0x0000FF) / 255.0
+            
+        } else if length == 8 {
+            r = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
+            g = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
+            b = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
+            a = CGFloat(rgb & 0x000000FF) / 255.0
+        }
+        self.init(red: r, green: g, blue: b, alpha: a)
+    }
+    
+}
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(
+            red:   .random(),
+            green: .random(),
+            blue:  .random(),
+            alpha: 1.0
+        )
+    }
+}
+
+
+extension UIImage {
+    func getAverageColor(onCompletion: @escaping (UIColor) -> Void) {
+        guard let inputImage = CIImage(image: self) else { return  }
+        
+        let extentVector = CIVector(x: inputImage.extent.origin.x, y: inputImage.extent.origin.y, z: inputImage.extent.size.width, w: inputImage.extent.size.height)
+        guard let filter = CIFilter(name: "CIAreaAverage", parameters: [kCIInputImageKey: inputImage, kCIInputExtentKey: extentVector]) else { return  }
+        guard let outputImage = filter.outputImage else { return  }
+        
+        var bitmap = [UInt8](repeating: 0, count: 4)
+        let context = CIContext(options: [.workingColorSpace: kCFNull!])
+        context.render(outputImage, toBitmap: &bitmap, rowBytes: 4, bounds: CGRect(x: 0, y: 0, width: 1, height: 1), format: .RGBA8, colorSpace: nil)
+        onCompletion(UIColor(red: CGFloat(bitmap[0]) / 255, green: CGFloat(bitmap[1]) / 255 , blue: CGFloat(bitmap[2]) / 255, alpha: 1))
+    }
+}
--- Calculator_UI/Extensions/FoundationExtensions.swift
+++ Calculator_UI/Extensions/FoundationExtensions.swift
@@ -5,4 +5,33 @@
 //  Created by Shubham on 13/07/21.
 //
 
-import Foundation
+import UIKit
+
+extension Double {
+    func convert(fromRange: (Double, Double), toRange: (Double, Double)) -> Double {
+        var value = self
+        value -= fromRange.0
+        value /= Double(fromRange.1 - fromRange.0)
+        value *= toRange.1 - toRange.0
+        value += toRange.0
+        return value
+    }
+    func clean(places: Int) -> String {
+        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(format: "%.\(places)f", self)
+    }
+}
+
+
+extension Int {
+    func appendZeros() -> String {
+        if (self < 10) {
+            return "0\(self)"
+        } else {
+            return "\(self)"
+        }
+    }
+    
+    func degreeToRadians() -> CGFloat {
+        return  (CGFloat(self) * .pi) / 180
+    }
+}
