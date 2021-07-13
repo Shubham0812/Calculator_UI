64d2da20d34243fd9d935a93e56165e3c1744b4f
Shubham0812 <singh.shubham0812@gmail.com>
2021-07-13 18:36:58 +0530

    String Extensions added

---
+++ Calculator_UI/Extensions/StringExtensions.swift
@@ -0,0 +1,35 @@
+//
+//  StringExtensions.swift
+//  Calculator_UI
+//
+//  Created by Shubham on 13/07/21.
+//
+
+import Foundation
+
+extension String {
+    
+    var length: Int {
+        return count
+    }
+    
+    subscript (i: Int) -> String {
+        return self[i ..< i + 1]
+    }
+    
+    func substring(fromIndex: Int) -> String {
+        return self[min(fromIndex, length) ..< length]
+    }
+    
+    func substring(toIndex: Int) -> String {
+        return self[0 ..< max(0, toIndex)]
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
+                                            upper: min(length, max(0, r.upperBound))))
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+        return String(self[start ..< end])
+    }
+}
